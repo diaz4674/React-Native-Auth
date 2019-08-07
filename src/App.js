1ad21aa120b2ff69bf74f3fc7436fee59edc12b4
Miguel Diaz <37884809+diaz4674@users.noreply.github.com>
2019-08-06 20:47:37 -0700

    added components

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+
+class App extends React.Component {
+    render(){
+        return (
+            <View>
+                <Text>
+                    An App
+                </Text>
+            </View>
+        )
+    }
+}
+
+export default App
