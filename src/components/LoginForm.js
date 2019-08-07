f4a106eb8f88759a404ea9190c5880a8c38c31d7
Miguel Diaz <37884809+diaz4674@users.noreply.github.com>
2019-08-06 23:00:01 -0700

    password input works

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,7 +4,7 @@ import {Button, CardSection, Card, Input} from './common'
 class LoginForm extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {email: ''}
+        this.state = {email: '', password: ''}
     }
 
 
@@ -16,8 +16,17 @@ class LoginForm extends React.Component {
                         label = "Email"
                         placeholder = "user@gmail.com"
                         value = {this.state.email}
-                        onChangeText = {email => this.setState({email: email})}
+                        onChangeText = {email => this.setState({email})}
                      />
+                </CardSection>
+                <CardSection>
+                    <Input
+                        secureTextEntry
+                        placeholder='password'
+                        label = "Password"
+                        value = {this.state.password}
+                        onChangeText = {password => this.setState({password})}
+                    />
                 </CardSection>
                 <CardSection>
                     <Button>
